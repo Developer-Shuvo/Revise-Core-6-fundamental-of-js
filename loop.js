b60ff1d6ff473b6301fe4859030318e67a3733d0
SHUVO <im.computer.engineeer@gmail.com>
2025-05-23 22:29:39 +0600

    Loop
    *For loop
    *While loop
    *For of
    *Loop Through an Array

---
+++ loop.js
@@ -0,0 +1,77 @@
+// Here All Loop Available
+
+for (let a = 1; a<=5; a++){   //basic for loop
+console.log("Result is:=", a);
+}
+
+
+
+// 100 times print
+for (let i=0; i<=10; i++){
+    console.log("I love Programming");  // 100 times print
+
+}
+
+// Loop Through an Array
+let nameList = ["Amin", "Zamin", "Halim","Abdullah"];
+
+for(let a=0; a<nameList.length; a++){                 
+    console.log(nameList[a]);
+
+}
+
+//while loop
+let count = 5;
+
+while (count <= 10) {            //while loop
+  console.log("Count:", count);
+  count++;
+}
+
+
+
+// ***************For ... of loop***********************
+
+let bikes = ["Bmw","Kawasaki","Ducati","Honda"];
+for(let bike of bikes){
+    console.log("Brand is:",bike);
+}
+
+// **************************************
+
+let fruits =["Mango","Orange","Jackfruit","Grape","Lemon","Cucumber"];
+
+for(result of fruits){
+    console.log('Fruit Name is:=', result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ______________________Task____________________________
+
+// Prints numbers from 1 to 10?
+
+for (let a=1; a<=10; a++){
+    console.log(a)
+}
+
+// Loops through this array and prints each item:
+let animals = ["cat", "dog", "cow", "tiger"];
+
+for(let a=0; a<animals.length; a++){
+    console.log(animals[a]);
+}
+
