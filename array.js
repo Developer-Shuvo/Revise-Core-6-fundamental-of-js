7f8e3b1d3f2a4c553aec8138d376d05c934f28e8
SHUVO <im.computer.engineeer@gmail.com>
2025-05-23 21:04:22 +0600

    array declare

---
+++ array.js
@@ -0,0 +1,29 @@
+// let fruits = ["Apple", "Banana", "Grape", "Litchi","PineApple"];
+
+// fruits.push("Coconut");     // push in the array
+// fruits.pop();   //Removes Last item
+// console.log(fruits);
+// console.log(fruits[1]);    //handle by index number
+// console.log(fruits.length);    //find the length of array   
+
+
+
+
+// for (let i = 0; i <fruits.length; i++){  //Loop through of an array
+// console.log (fruits[i]);
+// }
+
+
+
+
+let favouriteFoods = ["Chiken", "Kacchi","Matha","Khichuri"];
+
+favouriteFoods.push("Fish Fry")  //push
+favouriteFoods.pop(); //pop
+
+for (let i =0; i<favouriteFoods.length; i++){
+    console.log(favouriteFoods[i]);   //Loop through
+}
+
+console.log(favouriteFoods);
+
